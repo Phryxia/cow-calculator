44d5dc0e9c3e25b4d4fd09babaaca39073dbf482
sekyu.kwon <sekyu.kwon@navercorp.com>
2022-08-18 15:44:33 +0900

    Implement persisting configuration and refactor using event based.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Cow } from './Cow'
-import Price from './Price'
-import { loadPrice, savePrice } from './storage'
+import { price } from './dom/configuration'
 import './styles.css'
 
 const $input = document.querySelector('#input') as HTMLTextAreaElement
@@ -10,8 +9,6 @@ const $priceOutput = document.querySelector(
 ) as HTMLTextAreaElement
 const $button = document.querySelector('#calculate-button') as HTMLInputElement
 
-const pricer = loadPrice()
-
 $button.onclick = () => {
   // 안전 장치
   if (isNotCompleted()) {
@@ -19,17 +16,14 @@ $button.onclick = () => {
     return
   }
 
-  // DOM에서 값 불러오기
-  setPricer(pricer)
-
   // 계산하기
   const cows = parseCows()
 
   // 계산과정 렌더
-  const prices = renderCalculationProcess(cows)
+  const renderedCalculations = renderCalculationProcess(cows)
 
   // 결과값 렌더
-  renderFinalOutcomes(prices)
+  renderFinalOutcomes(renderedCalculations)
 }
 
 function isNotCompleted(): boolean {
@@ -45,90 +39,49 @@ function isNotCompleted(): boolean {
   return warn
 }
 
-function setPricer(pricer: Price) {
-  pricer.priceCalfF4month = parseInt(
-    (document.getElementById('price-calf-f-4~5month') as HTMLInputElement)
-      .value,
-  )
-  pricer.priceCalfM4month = parseInt(
-    (document.getElementById('price-calf-m-4~5month') as HTMLInputElement)
-      .value,
-  )
-  pricer.priceCalfF6month = parseInt(
-    (document.getElementById('price-calf-f-6~7month') as HTMLInputElement)
-      .value,
-  )
-  pricer.priceCalfM6month = parseInt(
-    (document.getElementById('price-calf-m-6~7month') as HTMLInputElement)
-      .value,
-  )
-  pricer.priceCowF = parseInt(
-    (document.getElementById('price-cow-f') as HTMLInputElement).value,
-  )
-  pricer.priceCowM = parseInt(
-    (document.getElementById('price-cow-m') as HTMLInputElement).value,
-  )
-  pricer.priceCowC = parseInt(
-    (document.getElementById('price-cow-c') as HTMLInputElement).value,
-  )
-  pricer.stdCuffWeightF = parseFloat(
-    (document.getElementById('standard-f') as HTMLInputElement).value,
-  )
-  pricer.stdCuffWeightM = parseFloat(
-    (document.getElementById('standard-m') as HTMLInputElement).value,
-  )
-  savePrice(pricer)
-}
-
 function parseCows(): Cow[] {
   const cows = [] as Cow[]
 
-  const content = $input.value.trim()
-  const lines = content.split('\n')
+  const contents = $input.value.trim()
+  const lines = contents.split('\n')
 
   // 입력 양식에서
   for (let lineNum = 0; lineNum < lines.length; lineNum += 8) {
-    // header인 경우
-    if (lines[lineNum] === '축종') continue
-    // footer인 경우
-    else if (lines[lineNum] === '금회 신청액') continue
+    // header/footer인 경우
+    if (['축종', '금회 신청액'].includes(lines[lineNum])) continue
 
-    const cow = new Cow({
-      name: lines[lineNum],
-      type: lines[lineNum + 1],
-      id: lines[lineNum + 2],
-      sex: lines[lineNum + 3],
-      age: lines[lineNum + 4],
-      weight: lines[lineNum + 5],
-      pregnancy: lines[lineNum + 6],
-    })
+    try {
+      const cow = new Cow({
+        name: lines[lineNum],
+        type: lines[lineNum + 1],
+        id: lines[lineNum + 2],
+        sex: lines[lineNum + 3],
+        age: lines[lineNum + 4],
+        weight: lines[lineNum + 5],
+        pregnancy: lines[lineNum + 6],
+      })
 
-    cows.push(cow)
+      cows.push(cow)
+    } catch (e) {
+      console.warn(e)
+    }
   }
 
   return cows
 }
 
-/**
- * 계산과정을 렌더링하여 DOM에 출력하고, 계산결과값을 배열로 반환한다.
- * @param cows 파싱한 소
- */
 function renderCalculationProcess(cows: Cow[]): number[] {
   const prices = [] as number[]
   $output.value = ''
   for (const cow of cows) {
-    const [sentence, price] = pricer.renderCow(cow)
+    const [sentence, value] = price.renderCow(cow)
     $output.value += sentence + '\n'
 
-    prices.push(price)
+    prices.push(value)
   }
   return prices
 }
 
-/**
- * 계산결과값을 렌더링하여 DOM에 출력한다.
- * @param prices 계산결과값
- */
 function renderFinalOutcomes(prices: number[]): void {
   $priceOutput.value = ''
   for (const price of prices) {
