477c7bd8d596d719e84f1035058ed9179b2d2bd7
sekyu.kwon <sekyu.kwon@navercorp.com>
2022-08-18 12:55:29 +0900

    Apply postcss and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'index.js',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'babel-loader'
-      }
-    ]
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  }
+    extensions: ['.ts', '.js', '.css'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
