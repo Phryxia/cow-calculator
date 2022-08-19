6b7fb181451db2cc888319b5a335e0132435c6d4
sekyu.kwon <sekyu.kwon@navercorp.com>
2022-08-19 14:02:42 +0900

    Add public resource copy to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -27,6 +28,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
     }),
   ],
   devServer: {
