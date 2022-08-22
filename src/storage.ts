e8be5babf9e51b56fe16bc77cd6f71acabc203c1
sekyu.kwon <sekyu.kwon@navercorp.com>
2022-08-22 16:26:27 +0900

    Fix crash when there is no stored value

--- src/storage.ts
+++ src/storage.ts
@@ -4,7 +4,19 @@ const PRICE = 'PRICE'
 
 export function loadPrice(): Price {
   const jsonString = window.localStorage.getItem(PRICE)
-  const price = jsonString ? JSON.parse(jsonString) : {}
+  const price: Price = jsonString
+    ? JSON.parse(jsonString)
+    : {
+        priceCalfF4month: 0,
+        priceCalfF6month: 0,
+        priceCalfM4month: 0,
+        priceCalfM6month: 0,
+        priceCowC: 0,
+        priceCowF: 0,
+        priceCowM: 0,
+        stdCuffWeightF: 137.1,
+        stdCuffWeightM: 160.4,
+      }
   return price
 }
 
